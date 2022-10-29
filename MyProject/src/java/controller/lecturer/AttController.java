67877e251bc9c20185c0c57aa29adf9c9445bbc0
Kasa <103961248+Kawoasa@users.noreply.github.com>
2022-10-30 00:40:44 +0700

    Update AttController.java

--- MyProject/src/java/controller/lecturer/AttController.java
+++ MyProject/src/java/controller/lecturer/AttController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.lecturer;
 
 import dal.SessionDBContext;
@@ -16,17 +15,13 @@ import model.Session;
 import model.Student;
 import util.DateTimeHelper;
 
-/**
- *
- * @author sonnt
- */
+
 public class AttController extends HttpServlet {
-   
-    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -34,21 +29,22 @@ public class AttController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         int sesid = Integer.parseInt(request.getParameter("id"));
         SessionDBContext sesDB = new SessionDBContext();
         Session ses = sesDB.get(sesid);
         request.setAttribute("ses", ses);
-        if(DateTimeHelper.getDaystoCurrent(ses.getDate())>=2)
-            response.getWriter().println("this session is out of date");
-        else if(DateTimeHelper.getDaystoCurrent(ses.getDate())< 0)
-            response.getWriter().println("this session is not yet started");
-        else
+//        if(DateTimeHelper.getDaystoCurrent(ses.getDate())>=2)
+//            response.getWriter().println("this session is out of date");
+//        else if(DateTimeHelper.getDaystoCurrent(ses.getDate())< 0)
+//            response.getWriter().println("this session is not yet started");
+//        else
         request.getRequestDispatcher("../view/lecturer/att.jsp").forward(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -56,26 +52,27 @@ public class AttController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         Session ses = new Session();
         ses.setId(Integer.parseInt(request.getParameter("sesid")));
         String[] stdids = request.getParameterValues("stdid");
         for (String stdid : stdids) {
-            Attandance a =new Attandance();
+            Attandance a = new Attandance();
             Student s = new Student();
             a.setStudent(s);
-            a.setDescription(request.getParameter("description"+stdid));
-            a.setPresent(request.getParameter("present"+stdid).equals("present"));
+            a.setDescription(request.getParameter("description" + stdid));
+            a.setPresent(request.getParameter("present" + stdid).equals("present"));
             s.setId(Integer.parseInt(stdid));
             ses.getAttandances().add(a);
         }
         SessionDBContext db = new SessionDBContext();
         db.update(ses);
-        response.sendRedirect("takeatt?id="+ses.getId());
+        response.sendRedirect("takeatt?id=" + ses.getId());
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
