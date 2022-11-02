127112754127c174986c407e3e5c0ac9d0ac3ec3
Kasa <103961248+Kawoasa@users.noreply.github.com>
2022-11-03 01:38:58 +0700

    Update Student.java

--- MyProject/src/java/model/Student.java
+++ MyProject/src/java/model/Student.java
@@ -6,23 +6,22 @@ package model;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author sonnt
- */
+
 public class Student {
-    private int id;
+    private String id;
     private String name;
     private ArrayList<Group> groups = new ArrayList<>();
     private ArrayList<Attandance> atts = new ArrayList<>();
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
+
+    
 
     public String getName() {
         return name;
